735a0ba69b5b656a2437e1986aefe4cd91b74a85
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-02-20 19:12:12 +0100

    [YESV-0] Move to java 9.

--- src/main/java/be/yildizgames/server/game/GameEngine.java
+++ src/main/java/be/yildizgames/server/game/GameEngine.java
@@ -1,44 +1,44 @@
 /*
  * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
  *
- *  Copyright (c) 2018 Grégory Van den Borre
+ * Copyright (c) 2018 Grégory Van den Borre
  *
- *  More infos available: https://www.yildiz-games.be
+ * More infos available: https://www.yildiz-games.be
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- *  documentation files (the "Software"), to deal in the Software without restriction, including without
- *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- *  of the Software, and to permit persons to whom the Software is furnished to do so,
- *  subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all copies or substantial
- *  portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
- *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE  SOFTWARE.
  */
 
-package be.yildiz.server.game;
+package be.yildizgames.server.game;
 
-import be.yildiz.module.network.protocol.NetworkMessage;
-import be.yildiz.module.network.server.Server;
-import be.yildiz.module.network.server.SessionListener;
-import be.yildiz.module.network.server.SessionManager;
-import be.yildiz.module.physics.PhysicEngine;
-import be.yildiz.server.physic.ServerPhysicEngine;
-import be.yildiz.server.physic.ServerWorld;
-import be.yildiz.shared.game.engine.AbstractGameEngine;
-import be.yildiz.shared.game.engine.DataInitializer;
-import be.yildiz.shared.game.engine.Initializable;
 import be.yildizgames.common.gameobject.CollisionListener;
+import be.yildizgames.common.logging.LogFactory;
 import be.yildizgames.common.model.PlayerId;
 import be.yildizgames.common.model.Version;
+import be.yildizgames.module.network.protocol.NetworkMessage;
+import be.yildizgames.module.network.server.Server;
+import be.yildizgames.module.network.server.SessionListener;
+import be.yildizgames.module.network.server.SessionManager;
+import be.yildizgames.server.physic.ServerPhysicEngine;
+import be.yildizgames.server.physic.ServerWorld;
+import be.yildizgames.shared.game.engine.AbstractGameEngine;
+import be.yildizgames.shared.game.engine.DataInitializer;
+import be.yildizgames.shared.game.engine.Initializable;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Server side implementation for the game engine.
@@ -47,7 +47,7 @@ import org.slf4j.LoggerFactory;
  */
 public final class GameEngine extends AbstractGameEngine implements ResponseSender, AutoCloseable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GameEngine.class);
+    private static final Logger LOGGER = LogFactory.getInstance().getLogger(GameEngine.class);
 
     /**
      * Frame limiter.
@@ -79,16 +79,15 @@ public final class GameEngine extends AbstractGameEngine implements ResponseSend
     /**
      * Full constructor, initialize the physic and the network engines, create the data manager.
      * Also add a shutdown hook to handle gracefully the termination signal.
-     * @param physicEngine Associated physic engine.
      * @param sessionManager Session manager.
      * @param server Network server.
      * @param version Game version.
      */
     //@effect Create a new engine.
-    public GameEngine(PhysicEngine physicEngine, SessionManager sessionManager, Server server, Version version) {
+    public GameEngine(SessionManager sessionManager, Server server, Version version) {
         super(version);
         LOGGER.info("Starting server game engine...");
-        this.physicEngine = new ServerPhysicEngine(physicEngine);
+        this.physicEngine = new ServerPhysicEngine();
         this.activeWorld = this.physicEngine.createWorld();
         this.initializer = new DataInitializer();
         this.sessionManager = sessionManager;
--- src/main/java/be/yildizgames/server/physic/ServerWorld.java
+++ src/main/java/be/yildizgames/server/physic/ServerWorld.java
@@ -1,43 +1,44 @@
 /*
  * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
  *
- *  Copyright (c) 2018 Grégory Van den Borre
+ * Copyright (c) 2018 Grégory Van den Borre
  *
- *  More infos available: https://www.yildiz-games.be
+ * More infos available: https://www.yildiz-games.be
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- *  documentation files (the "Software"), to deal in the Software without restriction, including without
- *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- *  of the Software, and to permit persons to whom the Software is furnished to do so,
- *  subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all copies or substantial
- *  portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
- *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE  SOFTWARE.
  */
 
-package be.yildiz.server.physic;
+package be.yildizgames.server.physic;
 
-import be.yildiz.module.physics.GhostObject;
-import be.yildiz.module.physics.Gravity;
-import be.yildiz.module.physics.KinematicBody;
-import be.yildiz.module.physics.PhysicMesh;
-import be.yildiz.module.physics.PhysicWorld;
-import be.yildiz.module.physics.RaycastResult;
-import be.yildiz.module.physics.StaticBody;
-import be.yildiz.module.physics.World;
-import be.yildiz.server.gameobject.ServerGameEntity;
+import be.yildizgames.server.gameobject.ServerGameEntity;
 import be.yildizgames.common.gameobject.CollisionListener;
 import be.yildizgames.common.geometry.Point3D;
 import be.yildizgames.common.model.EntityId;
 import be.yildizgames.common.shape.Box;
 import be.yildizgames.common.shape.Sphere;
+import be.yildizgames.module.physics.GhostObject;
+import be.yildizgames.module.physics.Gravity;
+import be.yildizgames.module.physics.KinematicBody;
+import be.yildizgames.module.physics.PhysicMesh;
+import be.yildizgames.module.physics.PhysicWorld;
+import be.yildizgames.module.physics.RaycastResult;
+import be.yildizgames.module.physics.StaticBody;
+import be.yildizgames.module.physics.World;
 
 /**
  * Bullet implementation for the ServerWorld_todelete.
@@ -232,8 +233,9 @@ public final class ServerWorld implements World {
     }
 
     @Override
-    public void setGravity(final Gravity newGravity) {
+    public World setGravity(final Gravity newGravity) {
         this.physicWorld.setGravity(newGravity);
+        return this;
     }
 
     @Override
